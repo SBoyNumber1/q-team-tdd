94875cf7e7924f1bd5408713f0b361e93e923e4f
ElwooD07 <elwood0777@gmail.com>
2018-09-27 20:33:33 +0300

    4: /!\ Changed original question, prepared source code for direct start /!\

--- tdd_intro/demo/04_timer/test.cpp
+++ tdd_intro/demo/04_timer/test.cpp
@@ -1,6 +1,15 @@
 /*
 Timer
-Implement a timer class that implements a following interface:
+Implement a timer class that implements the interface ITimer which is defined below.
+Follow this specification:
+
+Timer allows to determine if specific period of time expired or not since Start was called last time
+Timer allows to determine how much time left till period is expired
+Timer can be started again any time, no metter in what state it is
+*/
+
+#include <gtest/gtest.h>
+#include <chrono>
 
 typedef std::chrono::high_resolution_clock Clock;
 typedef Clock::duration Duration;
@@ -14,11 +23,3 @@ public:
   virtual bool IsExpired() const = 0;
   virtual Duration TimeLeft() const = 0;
 };
-Followed by this specification:
-
-Timer allows to determine if specific period of time expired or not since Start was called last time
-Timer allows to determine how much time left till period is expired
-Timer can be started again any time, no metter in what state it is
-*/
-
-#include <gtest/gtest.h>
