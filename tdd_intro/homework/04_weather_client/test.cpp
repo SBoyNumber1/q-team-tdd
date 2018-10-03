dc6559fd847c04bc8edbe2f72d87e96d5dd13e24
Denis Belyshev <belyshev.denis@apriorit.com>
2018-10-03 18:00:13 +0300

    4: /!\ Changed condition /!\

--- tdd_intro/homework/04_weather_client/test.cpp
+++ tdd_intro/homework/04_weather_client/test.cpp
@@ -1,43 +1,81 @@
 /*
-Fake Weather Client
+Weather Client
 
-You are going to develop a program that gets the statistics about weather in the cities using information from a certain server. Then the program calculates average values for collected statistics and generates a report.
+You are going to develop a program that gets the statistics about weather in the current city
+using information from a certain server. The goal is to calculate statistics using the data from weather server.
 
-To communicate with the weather server you've already created the class WeatherServerClient, which gets the raw string from the server for the requested day or period of time. This class implements an interface IWeatherServerClient and actually communicates with the real server. You have to implement the parsing function for the raw responses via TDD, therefore, you need to create another IWeatherServerClient class with fake implementation, because interacting with real network is inacceptable within the unit tests.
-  
-The server answers with text of this format:
-    31.08.2018;03:00;20;181:5.1
-    31.08.2018;09:00;23;204:4.9
-    31.08.2018;15:00;33;193:4.3
-    31.08.2018;21:00;46;179:4.5
-    Where each line represents the time of the day and contains the next information: "<date>;<time>;<air_temperature_in_celsius>;<wind_direction_in_degrees>:<wind_speed>". Wind direction value may be in range from 0 to 359 inclusively, temperature may be negative.
+To communicate with the weather server you have to implement interface IWeatherServer,
+which provides the raw string from the real server for the requested day and time.
 
-Your program should parse the answers from weather server and collect the average values of temperature, wind direction and speed for the given period of time.
+The real server (i.e. "weather.com") gets the requests in this format:
+"<date>;<time>", for example:
+"31.08.2018;03:00"
 
-Stage 1: For the start, you have to implement the function which collect statistics for a single day for a given city.
-Stage 2 (optional): If you want to make your program more useful, implement the function which collects the statistics for the certain period of time.
+The server answers on requests with string like this:
+"20;181;5.1"
+This string contains the weather for the requested time and means next:
+"<air_temperature_in_celsius>;<wind_direction_in_degrees>:<wind_speed>".
+Wind direction value may be in range from 0 to 359 inclusively, temperature may be negative.
+
+The task:
+1. Implement fake server, because interacting with real network is inacceptable within the unit tests.
+To do this, you need to use real server responses. Fortunately, you've collected some results for the several dates from the weather server.
+Each line means "<request>" : "<response>":
+
+"31.08.2018;03:00" : "20;181;5.1"
+"31.08.2018;09:00" : "23;204;4.9"
+"31.08.2018;15:00" : "33;193;4.3"
+"31.08.2018;21:00" : "26;179;4.5"
+
+"01.09.2018;03:00" : "19;176;4.2"
+"01.09.2018;09:00" : "22;131;4.1"
+"01.09.2018;15:00" : "31;109;4.0"
+"01.09.2018;21:00" : "24;127;4.1"
+
+"02.09.2018;03:00" : "21;158;3.8"
+"02.09.2018;09:00" : "25;201;3.5"
+"02.09.2018;15:00" : "34;258;3.7"
+"02.09.2018;21:00" : "27;299;4.0"
+
+IMPORTANT:
+* Server returns empty string if request is invalid.
+* Real server stores weather only for times 03:00, 09:00, 15:00 and 21:00 for every date. Do not use other hours in a day.
+
+2. Implement IWeatherClient using fake server.
 */
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-#include <string>
 
-class IWeatherServerClient
+struct Weather
+{
+    short temperature = 0;
+    unsigned short windDirection = 0;
+    double windSpeed = 0;
+    bool operator==(const Weather& right)
+    {
+        return temperature == right.temperature &&
+               windDirection == right.windDirection &&
+               std::abs(windSpeed - right.windSpeed) < 0.01;
+    }
+};
+
+class IWeatherServer
 {
 public:
-    virtual ~IWeatherServerClient() { }
-    // Returns raw statistics for the given day
-    virtual std::string GetWeather(const std::string& city, const std::string& date) = 0;
-    // Returns raw statistics for the given period of time
-    virtual std::string GetWeather(const std::string& city, const std::string& startDate, const std::string& endDate) = 0;
+    virtual ~IWeatherServer() { }
+    // Returns raw response with weather for the given day and time in request
+    virtual std::string GetWeather(const std::string& request) = 0;
 };
 
-// Stage 1: Get the average weather statistics for the single day
-struct Average
+// Implement this interface
+class IWeatherClient
 {
-    // Your fields goes here
+public:
+    virtual ~IWeatherClient() { }
+    virtual double GetAverageTemperature(IWeatherServer& server, const std::string& date) = 0;
+    virtual double GetMinimumTemperature(IWeatherServer& server, const std::string& date) = 0;
+    virtual double GetMaximumTemperature(IWeatherServer& server, const std::string& date) = 0;
+    virtual double GetAverageWindDirection(IWeatherServer& server, const std::string& date) = 0;
+    virtual double GetMaximumWindSpeed(IWeatherServer& server, const std::string& date) = 0;
 };
-
-// Stage 2 (optional): Get the average weather statistics for the given period of time
-using Averages = std::map<std::string, Average>; // <date, Average>
-
