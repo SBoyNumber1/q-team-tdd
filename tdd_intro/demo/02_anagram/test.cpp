aa39d448678cb1ba304852c919051fe38fee48ab
Krestol <Krestol@i.ua>
2018-10-04 10:05:11 +0300

    first test RED

--- tdd_intro/demo/02_anagram/test.cpp
+++ tdd_intro/demo/02_anagram/test.cpp
@@ -6,3 +6,8 @@ Given "listen" and a list of candidates like "enlists" "google" "inlets" "banana
 _from http://exercism.io/_
 */
 #include <gtest/gtest.h>
+
+TEST (IsAnagrams, first_test)
+{
+    EXPECT_FALSE(IsAnagrams("", ""));
+}
