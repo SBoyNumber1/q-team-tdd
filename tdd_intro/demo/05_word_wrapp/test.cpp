abeace10ecb67316a551598544443e9d42059471
Denis Krestol <krestol.denis@apriorit.com>
2018-10-25 13:44:00 +0300

    added word_wrapp

---
+++ tdd_intro/demo/05_word_wrapp/test.cpp
@@ -0,0 +1,18 @@
+/*
+Write a function, that is given a string and a length limit, splits provided string into sequence of string,
+where length of each string is not more, than provided limit. If there are spaces under provided limit -
+last space should be used to wrapp this line. If there are no spaces - wrapp it on provided length limit.
+
+Example:
+When pos is specified, the search only includes sequences of characters that begin at or before position pos, ignoring any possible match beginning after pos
+
+"When pos is specified, the",
+"search only includes sequences",
+"of characters that begin at or",
+"before position pos, ignoring",
+"any possible match beginning",
+"after pos."
+*/
+
+#include <gtest/gtest.h>
+#include <cctype>
