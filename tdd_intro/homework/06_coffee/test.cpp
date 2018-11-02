0ff494598af0b5ee3d811b5f88b826fcbfae594b
Denis Belyshev <belyshev.denis@apriorit.com>
2018-11-02 11:19:38 +0200

    6: committed class work

--- tdd_intro/homework/06_coffee/test.cpp
+++ tdd_intro/homework/06_coffee/test.cpp
@@ -20,7 +20,7 @@ Implement worked coffee machine using ISourceOfIngredients to controll the proce
 class ISourceOfIngredients
 {
 public:
-    ~ISourceOfIngredients() {}
+    virtual ~ISourceOfIngredients() {}
     virtual void SetCupSize(int gram) = 0;
     virtual void AddWater(int gram, int temperature) = 0;
     virtual void AddSugar(int gram) = 0;
@@ -30,3 +30,90 @@ public:
     virtual void AddChocolate(int gram) = 0;
     virtual void AddCream(int gram) = 0;
 };
+
+
+enum Cup
+{
+    Normal,
+    Big
+};
+
+enum Coffee
+{
+    Americano
+};
+
+class MockSourceOfIngredients : public ISourceOfIngredients
+{
+public:
+    MOCK_METHOD1(SetCupSize, void(int));
+    MOCK_METHOD2(AddWater, void(int, int));
+    MOCK_METHOD1(AddSugar, void(int));
+    MOCK_METHOD1(AddCoffee, void(int));
+    MOCK_METHOD1(AddMilk, void(int));
+    MOCK_METHOD1(AddMilkFoam, void(int));
+    MOCK_METHOD1(AddChocolate, void(int));
+    MOCK_METHOD1(AddCream, void(int));
+};
+
+class CoffeeMachine
+{
+public:
+    CoffeeMachine(ISourceOfIngredients& source) : m_source(source)
+    {
+
+    }
+    void CreateCoffee(const Cup cup, const Coffee coffee)
+    {
+        m_source.AddCoffee(0);
+        m_source.SetCupSize(0);
+        m_source.AddWater(0, 0);
+    }
+private:
+    ISourceOfIngredients& m_source;
+};
+
+
+// Architecture
+// Class CoffeMachine
+// Class-Mock SourceOfIngredients
+
+// - americano: water & coffee 1:2 or 1:3. Water temp 60C
+
+// Tests list:
+// 1. americano + 100 gram = 1 coffe
+// 2. americano + 140 gram = 1 large coffee
+// 3. AddCoffee, SetCupSize and AddWater calls once
+// 4. Check parameters
+// 5. Same for each recipe
+
+TEST(CoffeeMachine, CoffemachineIsHere)
+{
+    MockSourceOfIngredients si;
+    CoffeeMachine cm(si);
+}
+
+TEST(CoffeeMachine, CallsImportantThings)
+{
+    MockSourceOfIngredients si;
+    CoffeeMachine cm(si);
+
+    EXPECT_CALL(si, AddCoffee(::testing::_)).Times(1);
+    EXPECT_CALL(si, SetCupSize(::testing::_)).Times(1);
+    EXPECT_CALL(si, AddWater(::testing::_, ::testing::_)).Times(1);
+
+    cm.CreateCoffee(Cup::Normal, Coffee::Americano);
+}
+
+//- americano: water & coffee 1:3 Water temp 60C
+TEST(CoffeeMachine, Americano)
+{
+    MockSourceOfIngredients si;
+    CoffeeMachine cm(si);
+
+    EXPECT_CALL(si, AddCoffee(75)).Times(1);
+    EXPECT_CALL(si, SetCupSize(100)).Times(1);
+    EXPECT_CALL(si, AddWater(25, 60)).Times(1);
+
+    cm.CreateCoffee(Cup::Normal, Coffee::Americano);
+}
