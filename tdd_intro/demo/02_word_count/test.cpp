6df6d9662084c3be0fedb94bf419185b626da54a
Mokych Andrey <mokych.andrey@apriorit.com>
2018-11-02 13:43:03 +0200

    WS#2 added word count demo project

---
+++ tdd_intro/demo/02_word_count/test.cpp
@@ -0,0 +1,19 @@
+/*
+Given a phrase, count the occurrences of each word in that phrase. Ignore whitespaces and punctual symbols
+For example for the input "olly olly in come free please please let it be in such manner olly"
+olly: 3
+in: 2
+come: 1
+free: 1
+please: 2
+let: 1
+it: 1
+be: 1
+manner: 1
+such: 1
+*/
+
+#include <gtest/gtest.h>
+#include <string>
+#include <map>
+
--- tdd_intro/demo/demo.pro
+++ tdd_intro/demo/demo.pro
@@ -4,5 +4,7 @@ SUBDIRS += \
     01_bob \
     01_fizz_buzz \
     02_anagram \
+    02_word_count \
+    #03_allergies \
     03_roman_numerals \
     04_timer
