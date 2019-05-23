14982604e8f795b831b2624b44014ba399d8c68d
Vladyslav Vizyr <vizyr.vladyslav@apriorit.com>
2019-05-23 09:00:48 +0300

    add uncompilable red test

--- tdd_intro/homework/01_leap_year/test.cpp
+++ tdd_intro/homework/01_leap_year/test.cpp
@@ -4,8 +4,8 @@ Given a year, report if it is a leap year.
 The tricky thing here is that a leap year in the Gregorian calendar occurs:
 
 on every year that is evenly divisible by 4
-  except every year that is evenly divisible by 100
-    unless the year is also evenly divisible by 400
+except every year that is evenly divisible by 100
+unless the year is also evenly divisible by 400
 
 For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
 
@@ -13,3 +13,8 @@ If your language provides a method in the standard library that does this look-u
 */
 
 #include <gtest/gtest.h>
+
+TEST(LeapYear, divisible_by_4)
+{
+    EXPECT_EQ(is_leap_year(1996), true);
+}
