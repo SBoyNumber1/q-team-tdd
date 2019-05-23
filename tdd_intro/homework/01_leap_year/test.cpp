a8b65060adc0dd1b31e44b5a2fe50f724cca649b
Vladyslav Vizyr <vizyr.vladyslav@apriorit.com>
2019-05-23 09:26:26 +0300

    make it green

--- tdd_intro/homework/01_leap_year/test.cpp
+++ tdd_intro/homework/01_leap_year/test.cpp
@@ -16,6 +16,11 @@ If your language provides a method in the standard library that does this look-u
 
 bool is_leap_year(int year)
 {
+    if (year % 400 == 0)
+    {
+        return true;
+    }
+
     if (year % 4 == 0 && year % 100 != 0)
     {
         return true;
