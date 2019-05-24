7aae1fc34cb58f75df13d509fdfb90962bfb4824
kacejot <kacejot@fex.net>
2019-05-24 12:58:06 +0300

    Corrected code to make test green

--- tdd_intro/homework/01_leap_year/test.cpp
+++ tdd_intro/homework/01_leap_year/test.cpp
@@ -16,6 +16,10 @@ If your language provides a method in the standard library that does this look-u
 
 bool isLeapYear(int year)
 {
+    if (year % 100 == 0) {
+        return false;
+    }
+
     if (year % 4 == 0) {
         return true;
     }
