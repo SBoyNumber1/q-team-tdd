8d16d5487ec2f7808a7a3be7900b864ece0c7dbf
Vladyslav Vizyr <vizyr.vladyslav@apriorit.com>
2019-06-04 10:56:55 +0300

    red test empty cant be ternary

--- tdd_intro/homework/02_ternary_numbers/test.cpp
+++ tdd_intro/homework/02_ternary_numbers/test.cpp
@@ -16,3 +16,13 @@ The last place in a ternary number is the 1's place. The second to last is the 3
 
 If your language provides a method in the standard library to perform the conversion, pretend it doesn't exist and implement it yourself.
 */
+
+bool can_be_ternary(const std::string& input)
+{
+    return true;
+}
+
+TEST(TernaryNumbers, EmptyCantBeTernary)
+{
+    EXPECT_FALSE(can_be_ternary(""));
+}
