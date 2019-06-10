fa275d8166e1f49956e46c14fd9e771dc1c4a9a1
Vladyslav Vizyr <vizyr.vladyslav@apriorit.com>
2019-06-10 11:21:39 +0300

    add red test for one word

--- tdd_intro/demo/02_word_count/test.cpp
+++ tdd_intro/demo/02_word_count/test.cpp
@@ -17,12 +17,31 @@ such: 1
 #include <string>
 #include <map>
 
-std::map<std::string, int> count_words(const std::string& /*input*/)
+
+/*
+ * List of tests:
+ *
+ * Empty string: done
+ * With punctual symbols:
+ * With whitespaces:
+ * With whitespaces and pubctual:
+ * One word:
+ * One word twice:
+ *
+ */
+
+std::map<std::string, int> count_words(const std::string& input)
 {
     return {};
 }
 
-TEST(WordCount, idk)
+TEST(WordCount, empty_string)
 {
     EXPECT_TRUE(count_words("").empty());
+}
+
+TEST(WordCount, one_word)
+{
+    std::map<std::string, int> expected = {{"one", 1}};
+    EXPECT_EQ(expected, count_words("one"));
 }
